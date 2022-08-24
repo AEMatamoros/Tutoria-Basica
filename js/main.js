b7d4ba2fa41be7704c3094c4ad62f2dca2d5020c
AEMatamoros <alexismatamoors1996888@gmail.com>
2022-08-23 21:05:22 -0600

    First Commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+let games = [
+  { id: 1, name: "Game 1", description: "lorem ipsum dolorum" },
+  { id: 2, name: "Game 2", description: "lorem ipsum dolorum" },
+  { id: 3, name: "Game 3", description: "lorem ipsum dolorum" },
+  { id: 4, name: "Game 4", description: "lorem ipsum dolorum" },
+  { id: 5, name: "Game 5", description: "lorem ipsum dolorum" },
+  { id: 6, name: "Game 6", description: "lorem ipsum dolorum" },
+  { id: 7, name: "Game 7", description: "lorem ipsum dolorum" },
+  { id: 8, name: "Game 8", description: "lorem ipsum dolorum" },
+  { id: 9, name: "Game 9", description: "lorem ipsum dolorum" },
+  { id: 10, name: "Game 10", description: "lorem ipsum dolorum" },
+];
+
+// Exportar
+export default games;
